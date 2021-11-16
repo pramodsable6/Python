9213262a89222e0701885d87807b940ac8cc3133
Pramod Sable <51992673+pramodsable6@users.noreply.github.com>
2021-11-16 12:44:43 +0530

    Update contains_common_item.py

--- Arrays/contains_common_item.py
+++ Arrays/contains_common_item.py
@@ -10,7 +10,7 @@ def contains_common_item(arr1, arr2):
         else:
             print('False')
 
-comtains_common_item([1, 2, 3, 4, 5], [9, 6, 7, 8, 5])
+contains_common_item([1, 2, 3, 4, 5], [9, 6, 7, 8, 5])
 
 # Hence time complexity of contains_common_item() is O(a + b), by neglecting the constants rule
 # and its Space Complexity is O(a), since we are creating a new set variable
