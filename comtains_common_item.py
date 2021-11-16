58b2db91b5a4278c040efe957bd0ed72dc404501
Pramod Sable <51992673+pramodsable6@users.noreply.github.com>
2021-11-16 12:38:21 +0530

    initial commit

---
+++ comtains_common_item.py
@@ -0,0 +1,16 @@
+def comtains_common_item(arr1, arr2):
+    set1 = set()
+    for i in arr1:                              # O(a)
+        if i not in set1:                       # O(1)
+            set1.add(i)                         # O(1)
+    
+    for j in arr2:                              # O(b)
+        if j in set1:                           # O(1)
+            print('True')
+        else:
+            print('False')
+
+comtains_common_item([1, 2, 3, 4, 5], [9, 6, 7, 8, 5])
+
+# Hence time complexity of contains_common_item() is O(a + b), by neglecting the constants rule
+# and its Space Complexity is O(a), since we are creating a new set variable
