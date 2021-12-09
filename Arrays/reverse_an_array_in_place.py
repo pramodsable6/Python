bbd554ea4f01a48318009bb30c504662d7206f39
Pramod Sable <51992673+pramodsable6@users.noreply.github.com>
2021-12-10 00:05:44 +0530

    Create reverse_an_array_in_place.py

---
+++ Arrays/reverse_an_array_in_place.py
@@ -0,0 +1,17 @@
+def reverse(nums):
+    
+    start_index = 0  # index pointing to the first item
+    end_index = len(nums) - 1  # index pointing to the last item
+    
+    # This algorithm has a linear running time complexity - O(N)
+    while end_index > start_index:
+        # swap the items
+        nums[end_index], nums[start_index] = nums[start_index], nums[end_index]
+        start_index += 1
+        end_index -= 1
+    
+
+if __name__ == "__main__":
+    x = [1, 2, 3, 4, 5]    
+    reverse(x)
+    print(x)
