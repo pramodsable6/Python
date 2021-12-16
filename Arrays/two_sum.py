13135b4941d0f1eef9d3f32a968d548fcea07d57
Pramod Sable <51992673+pramodsable6@users.noreply.github.com>
2021-12-16 19:48:59 +0530

    Update two_sum.py

--- Arrays/two_sum.py
+++ Arrays/two_sum.py
@@ -1,10 +1,15 @@
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target: int):
         lookup = {}
         for i in range(len(nums)):
             if target - nums[i] in lookup:
                 return [lookup[target - nums[i]], i]
             else:
                 lookup[nums[i]] = i
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([1, 4, 6, 9, 3], 12))
