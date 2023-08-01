d171edcad41c3021dc280a1e817953f171adcae8
Pramod Sable <51992673+pramodsable6@users.noreply.github.com>
2023-08-01 22:54:21 +0530

    Create binary_search.py

---
+++ algorithms/binary_search.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/binary-search/
+def binary_search(nums, target):
+  i = 0
+  j = len(nums) - 1
+
+  while i <= j:
+	m = (i + j) // 2
+	if nums[m] == target:
+	  return m
+	elif nums[m] < target:
+	  i = m + 1
+	else:
+	  j = m - 1
+  return -1
